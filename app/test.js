0bbca094e8a77067004ac70b2abb0c5f0e25bb3f
Maurizio Battaghini <maurizio.battaghini@jobrapido.com>
2017-01-19 09:20:43 +0100

    code format

--- app/test.js
+++ app/test.js
@@ -2,30 +2,31 @@
 
 (function() {
 
-    var socket = new io();
+  var socket = new io();
 
-    var vm = new Vue({
-        el: '#chat',
-        data: {
-            messages: [{ text: 'primo messaggio' }],
-            form: '',
-            input: ''
-        },
-        methods: {
-            onSubmit: function(event) {
-                event.preventDefault();
-            },
-            submit: function(message) {
-                if (message) {
-                    socket.emit('chat-message', message);
-                    this.input = '';
-                }
-            }
+  var vm = new Vue({
+    el: '#chat',
+    data: {
+      messages: [{ text: 'primo messaggio' }],
+      form: '',
+      input: ''
+    },
+    methods: {
+      onSubmit: function(event) {
+        event.preventDefault();
+      },
+      submit: function(message) {
+        if (message) {
+          socket.emit('chat-message', message);
+          this.input = '';
         }
-    });
+      }
+    }
+  });
 
-    socket.on('chat-message', function(msg) {
-        vm.messages.push({ text: msg });
-    });
+  socket.on('chat-message', function(msg) {
+    vm.messages.push({ text: msg });
+  });
 
 })();
+
--- index.js
+++ index.js
@@ -11,19 +11,20 @@ app.use('/npm', express.static('node_modules'));
 app.use(express.static('app'));
 
 app.get('/', function(req, res) {
-    res.sendfile('./app/index.html');
+  res.sendfile('./app/index.html');
 });
 
 io.on('connection', function(socket) {
-    console.log('a user connected');
-    socket.on('disconnect', function() {
-        console.log('user disconnected');
-    });
-    socket.on('chat-message', function(msg) {
-        io.emit('chat-message', msg);
-    });
+  console.log('a user connected');
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+  socket.on('chat-message', function(msg) {
+    io.emit('chat-message', msg);
+  });
 });
 
 http.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
+  console.log('Node app is running on port', app.get('port'));
 });
+
