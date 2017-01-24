5f2d547d5ded00c8d830711bbba34d3a64ffeaf7
Maurizio Battaghini <maurizio.battaghini@jobrapido.com>
2017-01-24 10:10:20 +0100

    move firebase config into external files and read env data

---
+++ config/firebaseConfig.js
@@ -0,0 +1,12 @@
+'use strict';
+
+require('dotenv').config();
+
+module.exports = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID
+};
+
