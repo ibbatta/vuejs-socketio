7dd2de61c36c3a9609be1afb071ca5801aa5a65f
Maurizio Battaghini <maurizio.battaghini@gmail.com>
2017-11-23 09:11:33 +0100

    update template

---
+++ app/components/EmptyChat/EmptyChat.js
@@ -0,0 +1,13 @@
+const EmptyChat = {
+  template: `
+  <div class="card">
+    <div class="card-content">
+      <div class="content">
+        <p>No messages</p>
+      </div>
+    </div>
+  </div>
+  `,
+};
+
+module.exports = EmptyChat;
